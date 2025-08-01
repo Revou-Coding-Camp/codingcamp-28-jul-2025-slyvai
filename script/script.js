91525ccb608be6c31ddc42df72f2801003f74d4d
slyvia <slyviafams@gmail.com>
2025-08-01 18:49:59 +0700

    second commit

--- script/script.js
+++ script/script.js
@@ -4,5 +4,22 @@ const welcomeMessage = document.getElementById("welcome-message");
 if (userName && userName.trim() !== "") {
   welcomeMessage.textContent = `Hi ${userName} Welcome to our website!`;
 } else {
-  welcomeMessage.textContent = "Hi Guest Welcome to our website!";
+  welcomeMessage.textContent = "Hi Guest Welcome to my website!";
 }
+
+ document.getElementById('messageForm').addEventListener('submit', function(event) {
+      event.preventDefault(); 
+
+ 
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+  
+    document.getElementById('displayName').textContent = name;
+    document.getElementById('displayEmail').textContent = email;
+    document.getElementById('displayContent').textContent = message;
+
+    document.getElementById('displayMessage').style.display = 'block';
+
+ });
